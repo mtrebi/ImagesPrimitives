ee36607fdfa95c5043fb3f1e3e1ff9ccf5a18cf6
Mtrebi <marianotrebino@gmail.com>
2017-10-20 21:02:07 +0200

    Fixed score calculation to use total instead of avg

--- includes/Utils.h
+++ includes/Utils.h
@@ -19,33 +19,26 @@ public:
   }
 
   // Score calculation using root-mean-square-error
+  // TODO Alpha shit!!!
   static float Energy(const Image& target, const Image& current)  {
     long long r_diff = 0,
       g_diff = 0,
-      b_diff = 0;
+      b_diff = 0,
+      total_diff = 0;
     for (int x = 0; x < target.GetWidth(); ++x) {
       for (int y = 0; y < target.GetHeight(); ++y) {
         const RGBApixel target_color = target.GetPixel(x, y);
         const RGBApixel current_color = current.GetPixel(x, y);
-
+        // TODO ALIASING
         // Calculate diff between target value and current value and rise to second power to dlete negatives
         r_diff += pow((current_color.Red - target_color.Red), 2);
         g_diff += pow((current_color.Green - target_color.Green), 2);
         b_diff += pow((current_color.Blue - target_color.Blue), 2);
+        total_diff += r_diff + g_diff + b_diff;
       }
     }
-    // Divide by the number of samples
-    r_diff /= (target.GetSize());
-    g_diff /= (target.GetSize());
-    b_diff /= (target.GetSize());
 
-    // Calculate sqrt
-    r_diff = sqrt(r_diff);
-    g_diff = sqrt(g_diff);
-    b_diff = sqrt(b_diff);
-
-    // Average between tree colors components
-    return (r_diff + g_diff + b_diff) / 3;
+    return sqrt(total_diff / target.GetSize()) / 255;
   }
 
   // Score calculation using root-mean-square-error
