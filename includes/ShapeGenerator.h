4ef0aa321829137015d9bdfb0537fc7f0d89cc17
Mtrebi <marianotrebino@gmail.com>
2017-10-23 11:41:06 +0200

    Added alpha data member

--- includes/ShapeGenerator.h
+++ includes/ShapeGenerator.h
@@ -13,7 +13,7 @@ class ShapeGenerator {
 private:
   Image target_;
   RandomGenerator generator_;
-
+  int alpha_;
   std::shared_ptr<Shape> GenerateTriangle() const {
     // TODO RandomPoint between start, end and height
     return std::shared_ptr<Shape>(new Triangle(generator_.RandomPoint(), generator_.RandomPoint(), generator_.RandomPoint()));
@@ -34,29 +34,31 @@ private:
       return nullptr;
     }
   }
-public:
-  const int RANDOM_BEST = 10;
 
-
-  ShapeGenerator(const Image& image)
-    : target_(image), generator_(target_.GetHeight(), target_.GetWidth()) {
-
-  }
-
-  std::shared_ptr<Shape> Generate(const ShapeType type) const {
+  std::shared_ptr<Shape> Generate(const ShapeType type, const Image& current) const {
     std::shared_ptr<Shape> shape = ShapeConstructor(type);
     while (!shape->Valid()) {
       shape = ShapeConstructor(type);
     }
-    shape->SetColor(target_);
+    shape->SetColor(target_, current, alpha_);
     return shape;
+  }
+
+public:
+  const int RANDOM_BEST = 10;
+
+
+  ShapeGenerator(const Image& image, const int alpha)
+    : target_(image), generator_(target_.GetHeight(), target_.GetWidth()),
+      alpha_(alpha) {
+
   }
 
   std::shared_ptr<Shape> GenerateBest(const Image& current, const ShapeType type) const {
     float best_energy = std::numeric_limits<float>::max();
     std::shared_ptr<Shape> best_shape = nullptr;
     for (int i = 0; i < RANDOM_BEST; ++i) {
-      std::shared_ptr<Shape> new_shape = Generate(type);
+      std::shared_ptr<Shape> new_shape = Generate(type, current);
       const float new_energy = Utils::Energy(target_, current, new_shape);
 
       if (new_energy < best_energy) {
--- includes/ShapeMutator.h
+++ includes/ShapeMutator.h
@@ -5,24 +5,26 @@
 class ShapeMutator {
 private:
   Image target_;
-public:
-  const int MUTATIONS = 50;
-
-  ShapeMutator(const Image& image)
-    : target_(image) {
-
+  int alpha_;
+private:
+  void Mutate(std::shared_ptr<Shape> original, const Image& current) {
+    original->Mutate();
+    original->SetColor(target_, current, alpha_);
   }
 
-  void Mutate(std::shared_ptr<Shape> original) {
-    original->Mutate();
-    original->SetColor(target_);
+public:
+  const int MUTATIONS = 100;
+
+  ShapeMutator(const Image& image, const int alpha)
+    : target_(image), alpha_(alpha) {
+
   }
 
   void MutateBest(float& best_energy, const Image& current, std::shared_ptr<Shape> shape) {
     best_energy = std::numeric_limits<float>::max();
 
     for (int i = 0; i < MUTATIONS; ++i) {
-      Mutate(shape);
+      Mutate(shape, current);
 
       float new_energy = Utils::Energy(target_, current, shape);
       if (new_energy < best_energy) {
