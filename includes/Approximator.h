04f97a254bf59e7dce02a44f6b998f2602fd6367
Mtrebi <marianotrebino@gmail.com>
2017-10-23 20:03:25 +0200

    Implemented multiple hill climbers at the same time using a thread pool

--- includes/Approximator.h
+++ includes/Approximator.h
@@ -6,6 +6,7 @@
 #include "Utils.h"
 #include "ThreadPool.h"
 #include <memory>
+#include <functional>
 
 class Approximator {
 private:
@@ -17,66 +18,39 @@ private:
   ShapeGenerator shape_generator_;
   ShapeMutator shape_mutator_;
 
-  ThreadPool thread_pool_;
+  ctpl::thread_pool thread_pool_;
 
   void Init() {
     current_.SetBaseColor(target_.AverageColor());
     current_energy_ = Utils::Energy(target_, current_);
-    thread_pool_.init();
+  }
+  
+  std::shared_ptr<Shape> HillClimber(const Image& current) {
+    std::shared_ptr<Shape> new_shape = shape_generator_.GenerateBest(current, SHAPE_TYPE);
+    shape_mutator_.MutateBest(current, new_shape);
+    return new_shape;
   }
 
-  
-  /*
-  std::shared_ptr<Shape> HillClimber(const Image& current) {
-    float energy;
-    std::shared_ptr<Shape> new_shape = shape_generator_.GenerateBest(current, SHAPE_TYPE);
-    shape_mutator_.MutateBest(energy, current, new_shape);
-  }*/
-
-  /*
-  Image Move(float &best_energy, const Image& current) {
+  std::shared_ptr<Shape> BestHillClimber(const Image& current) {
     std::shared_ptr<Shape> best_shape;
     std::future<std::shared_ptr<Shape>> results[N_HC];
-    best_energy = std::numeric_limits<float>::max();
 
     for (int i = 0; i < N_HC; ++i) {
-      //float energy_ref;
-      //auto f = thread_pool_.submit(HillClimber, std::ref(energy_ref));
-      //results[i].future = f;
-      //results[i].energy = energy_ref;
+      results[i] = thread_pool_.push(std::bind(&Approximator::HillClimber, this, std::ref(current))); //
     }
 
     for (int i = 0; i < N_HC; ++i) {
       if (results[i].valid()) {
-        std::shared_ptr<Shape> shape = results[i].get();
-        // Get Shape Energy
-        if (shape_energy < best_energy) {
-          best_shape = shape;
-          best_energy = shape_energy;
+        auto new_shape = results[i].get();
+        if (!best_shape || new_shape->GetEnergy() < best_shape->GetEnergy()) {
+          best_shape = new_shape;
         }
-
-      }
-
-    }
-
-    Image next = current;
-    best_energy = std::numeric_limits<float>::max();
-    std::shared_ptr<Shape> best_shape;
-    for (int i = 0; i < N_HC; ++i) {
-      float energy;
-      std::shared_ptr<Shape> new_shape = shape_generator_.GenerateBest(current, SHAPE_TYPE);
-      shape_mutator_.MutateBest(energy, current, new_shape);
-      if (energy < best_energy) {
-        best_shape = new_shape;
-        best_energy = energy;
       }
     }
-    next.AddShape(best_shape);
-    return next;
+    return best_shape;
   }
-  */
-  
-  std::shared_ptr<Shape> Move(const Image& current) {
+
+  std::shared_ptr<Shape> SingleThreadBestHillClimber(const Image& current) {
     std::shared_ptr<Shape> best_shape;
     for (int i = 0; i < N_HC; ++i) {
       std::shared_ptr<Shape> new_shape = shape_generator_.GenerateBest(current, SHAPE_TYPE);
@@ -86,7 +60,7 @@ private:
       }
     }
     return best_shape;
-  }
+  
   // TODO Optimize score differencePartial
   // TODO Aliasing
 public:
@@ -95,14 +69,14 @@ public:
   const float MIN_ENERGY = 0.025f;
   static const int N_HC = 15;
   static const int ALPHA = 128;
-  static const int N_THREADS = 1;
+  static const int N_THREADS = 7;
 
   Approximator(const Image& target) :
     current_(target.GetWidth(), target.GetHeight(), target.GetBitDepth(), target.GetHDPI(), target.GetVDPI()),
     target_(target),
     shape_generator_(target_, ALPHA),
     shape_mutator_(target_, ALPHA),
-    thread_pool_(7) {
+    thread_pool_(N_THREADS) {
   }
 
   Image Run() {
@@ -115,7 +89,7 @@ public:
     while (current_energy_ > MIN_ENERGY && iterations > 0) {
       std::cout << "Iteration " << MAX_ITERATIONS - iterations << " - Score " << current_energy_ << std::endl;
 
-      std::shared_ptr<Shape> new_shape = Move(current_); 
+      std::shared_ptr<Shape> new_shape = BestHillClimber(current_); 
 
       if (!new_shape) {
         continue;
