e8c185d29cef4fc36e6566e44f6dc6a45ec405f8
Mtrebi <marianotrebino@gmail.com>
2017-10-31 20:10:46 +0100

    Fixed main to use new refactored Approximator

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
-#include "Image.h"
 #include "Approximator.h"
-#include <math.h>
 
 int main() {
   // Initialization
   Image target = Image("../assets/monalisa.bmp");
-  
-  // Approximate Image using Hill Climber algorithm
-  Image current = Approximator(target).Run();
-
-  // Write output
-  current.Export("output.bmp");
+  Approximator approximator(target);
+  approximator.Run();
 
   return 0;
 }
-
