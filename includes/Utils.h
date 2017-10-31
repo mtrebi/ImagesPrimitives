0e1259703ef78bbf38e09dc1796076c34c99653c
Mtrebi <marianotrebino@gmail.com>
2017-10-31 15:59:56 +0100

    Added partial Score calculation

--- includes/Utils.h
+++ includes/Utils.h
@@ -1,17 +1,46 @@
 #pragma once
 #include "Image.h"
-class Shape;
 #include "../lib/EasyBMP_1.06/EasyBMP.h"
 #include <memory>
-
+#include <vector>
+#include "Scanline.h"
 static class Utils {
 public:
+
+  static void Swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+  }
+
+  static RGBApixel AverageColor(const Image& image) {
+    long long r = 0,
+      g = 0,
+      b = 0;
+    for (int x = 0; x < image.GetWidth(); ++x) {
+      for (int y = 0; y < image.GetHeight(); ++y) {
+        const RGBApixel color = image.GetPixel(x, y);
+        r += color.Red;
+        g += color.Green;
+        b += color.Blue;
+      }
+    }
+
+    RGBApixel average_color;
+    average_color.Red = r / (image.GetWidth() * image.GetHeight());
+    average_color.Green = g / (image.GetWidth() * image.GetHeight());
+    average_color.Blue = b / (image.GetWidth() * image.GetHeight());
+    average_color.Alpha = 255;
+
+    return average_color;
+  }
+
   static int Clamp(const int value, const int min, const int max) {
-    if (value < min) {
+    if (value <= min) {
       return min;
     }
 
-    if (value > max) {
+    if (value >= max) {
       return max;
     }
 
@@ -19,79 +48,50 @@ public:
   }
 
   // Score calculation using root-mean-square-error
-  static float Energy(const Image& target, const Image& current)  {
+  static float DifferenceFull(const Image& target, const Image& current)  {
     long long total_diff = 0;
 
-    for (int x = 0; x < target.GetWidth(); ++x) {
-      for (int y = 0; y < target.GetHeight(); ++y) {
+    for (int y = 0; y < target.GetHeight(); ++y) {
+      for (int x = 0; x < target.GetWidth(); ++x) {
         const RGBApixel target_color = target.GetPixel(x, y);
         const RGBApixel current_color = current.GetPixel(x, y);
 
         // Calculate diff between target value and current value and rise to second power to dlete negatives
-        const long long r_diff = (current_color.Red - target_color.Red);
-        const long long g_diff = (current_color.Green - target_color.Green);
-        const long long b_diff = (current_color.Blue - target_color.Blue);
-        const long long a_diff = (current_color.Alpha - target_color.Alpha);
+        const long long r_diff = (target_color.Red - current_color.Red);
+        const long long g_diff = (target_color.Green - current_color.Green);
+        const long long b_diff = (target_color.Blue - current_color.Blue);
 
-        total_diff += r_diff * r_diff + 
-                      g_diff * g_diff + 
-                      b_diff * b_diff/* + 
-                      a_diff * a_diff*/;
+        total_diff += r_diff * r_diff +
+          g_diff * g_diff +
+          b_diff * b_diff;
       }
     }
 
-    return sqrt(total_diff / (target.GetSize() * 3)) / 255;
+    return sqrt(total_diff / (target.GetSize() * 3.0f)) / 255;
   }
 
-  // Score calculation using root-mean-square-error
-  static float Energy(const Image& target, Image current, const std::shared_ptr<Shape> shape) {
-    Utils::AddShape(current, shape);
-    shape->SetEnergy(Energy(target, current));
-    return shape->GetEnergy();
-  }
+  static float DifferencePartial(const Image& target, const Image& before, const Image& after, const float score, const std::vector<Scanline>& lines) {
+    long long total = pow(score * 255, 2) * target.GetHeight() * target.GetHeight() * 3;
 
+    for (const auto& line : lines) {
+      for (int x = line.X1; x <= line.X2; ++x) {
+        const RGBApixel target_color = target.GetPixel(x, line.Y);
+        const RGBApixel before_color = before.GetPixel(x, line.Y);
+        const RGBApixel after_color = after.GetPixel(x, line.Y);
 
-  static void AddShape(Image& image, const std::shared_ptr<Shape> shape) {
-    int sr, sg, sb, sa;
-    Utils::ScaleByAlpha(sr, sg, sb, sa, shape->GetColor());
+        long long dr1 = target_color.Red - before_color.Red;
+        long long dg1 = target_color.Green - before_color.Green;
+        long long db1 = target_color.Blue - before_color.Blue;
 
-    const int m = 0xFFFF;
-    const int ma = 0xFFFF;
-    const float as = (m - (sa * (ma / m))) * 257;
-    const int a = as;
+        long long dr2 = target_color.Red - after_color.Red;
+        long long dg2 = target_color.Green - after_color.Green;
+        long long db2 = target_color.Blue - after_color.Blue;
 
-    const BoundingBox bbox = shape->GetBBox();
-    for (int x = bbox.min.x; x < bbox.max.x; ++x) {
-      for (int y = bbox.min.y; y < bbox.max.y; ++y) {
-        if (shape->Contains(Point(x, y))) {
-          RGBApixel d = image.GetPixel(x, y);
-
-          RGBApixel new_color;
-          new_color.Red =   ((d.Red   * a + sr * ma) / m) >> 8;
-          new_color.Green = ((d.Green * a + sg * ma) / m) >> 8;
-          new_color.Blue =  ((d.Blue  * a + sb * ma) / m) >> 8;
-          new_color.Alpha = ((d.Alpha * a + sa * ma) / m) >> 8;
-
-          image.SetPixel(x, y, new_color);
-        }
+        total -= dr1*dr1 + dg1*dg1 + db1*db1;
+        total += dr2*dr2 + dg2*dg2 + db2*db2;
       }
     }
+    return sqrt(total / (target.GetSize() * 3.0f)) / 255;
   }
 
-  // Scale the rgb color components by the alpha component
-  static void ScaleByAlpha(int& sr, int& sg, int& sb, int& sa, const RGBApixel &c) {
-    sr = c.Red | (c.Red << 8);
-    sr *= c.Alpha;
-    sr /= 0xFF;
-
-    sg = c.Green | (c.Green << 8);
-    sg *= c.Alpha;
-    sg /= 0xFF;
-
-    sb = c.Blue | (c.Blue << 8);
-    sb *= c.Alpha;
-    sb /= 0xFF;
-
-    sa = c.Alpha | (c.Alpha << 8);
-  }
 };
