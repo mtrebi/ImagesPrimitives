42fe68889c828fe69a50e113ff6b1d1197f5abae
Mtrebi <marianotrebino@gmail.com>
2017-11-03 12:25:21 +0100

    Move AverageColor function to Utils

--- includes/Image.h
+++ includes/Image.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "../lib/EasyBMP_1.06/EasyBMP.h"
 #include <memory>
-#include "Shape.h"
 
 class Image {
 private:
@@ -11,10 +10,16 @@ public:
     bmp_.ReadFromFile(image_path.c_str());
   }
 
-  Image(const int width, const int height, const int bitdepth, const int hDPI, const int vDPI) {
+  Image(const RGBApixel& background_color, const int width, const int height, const int bitdepth, const int hDPI, const int vDPI) {
     bmp_.SetSize(width, height);
     bmp_.SetBitDepth(bitdepth);
     bmp_.SetDPI(hDPI, vDPI);
+
+    for (int i = 0; i < bmp_.TellWidth(); ++i) {
+      for (int j = 0; j < bmp_.TellHeight(); ++j) {
+        bmp_.SetPixel(i, j, background_color);
+      }
+    }
   }
   
   Image(const Image& image) {
@@ -36,12 +41,8 @@ public:
     return *this;
   }
 
+  bool Export(const std::string path) {     return bmp_.WriteToFile(path.c_str()); }
 
-  bool Export(const std::string path) { 
-    return bmp_.WriteToFile(path.c_str()); 
-  }
-
-  RGBApixel GetPixel(const int i, const int j) const { return bmp_.GetPixel(i, j); }
   int GetWidth() const { return bmp_.TellWidth(); }
   int GetHeight() const { return bmp_.TellHeight(); }
   int GetBitDepth() const { return bmp_.TellBitDepth(); }
@@ -49,38 +50,6 @@ public:
   int GetHDPI() const { return bmp_.TellHorizontalDPI(); }
   int GetSize() const { return bmp_.TellWidth() * bmp_.TellHeight(); }
 
-
-  void SetPixel(const int x, const int y, const RGBApixel& color) {
-    bmp_.SetPixel(x, y, color);
-  }
-
-  RGBApixel AverageColor() const {
-    long long r = 0,
-      g = 0,
-      b = 0;
-    for (int i = 0; i < bmp_.TellWidth(); ++i) {
-      for (int j = 0; j < bmp_.TellHeight(); ++j) {
-        const RGBApixel color = bmp_.GetPixel(i, j);
-        r += color.Red;
-        g += color.Green;
-        b += color.Blue;
-      }
-    }
-
-    RGBApixel average_color;
-    average_color.Red = r / GetSize();
-    average_color.Green = g / GetSize();
-    average_color.Blue = b / GetSize();
-    average_color.Alpha = 255;
-
-    return average_color;
-  }
-
-  void SetBaseColor(const RGBApixel& color) {
-    for (int i = 0; i < bmp_.TellWidth(); ++i) {
-      for (int j = 0; j < bmp_.TellHeight(); ++j) {
-        bmp_.SetPixel(i, j, color);
-      }
-    }
-  }
+  RGBApixel& GetPixel(const int i, const int j) const { return bmp_.GetPixel(i, j); }
+  void SetPixel(const int x, const int y, const RGBApixel& color) { bmp_.SetPixel(x, y, color); }
 };
